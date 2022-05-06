99652ac60efacdc43a9e16622af481e1e78789d6
mohammed issa <91820968+klashINkov@users.noreply.github.com>
2022-05-07 00:45:14 +0300

    Updated By Mohammed issa at 12:34 am in 6/5/2022

--- lib/main screens/Specialties And Subjects/Subject/Subject Syllabus.dart
+++ lib/main screens/Specialties And Subjects/Subject/Subject Syllabus.dart
@@ -1,86 +1,99 @@
+// Done
+
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart' ;
 
+// Start Of Home Class
 class Syllabus extends StatefulWidget
 {
 
-
   @override
   State < Syllabus > createState ( ) => _Syllabus (  ) ;
+
 }
+// End Of Home Class
+
 
 class _Syllabus extends State < Syllabus >
 {
+
   late PdfViewerController _pdfViewerController ;
   String ? url ;
-
 
   @override
   void initState ( )
   {
-    _pdfViewerController = PdfViewerController ( ) ;
 
+    _pdfViewerController = PdfViewerController ( ) ;
     super . initState ( ) ;
+
   }
 
+  // Start Of _Home Class
   @override
   Widget build ( BuildContext context )
   {
+
     return SafeArea
       (
 
         child : Scaffold
+        (
+
+
+          appBar : AppBar
           (
 
-            body : SfPdfViewer . network
-            (
-              url! ,
-              controller : _pdfViewerController ,
-            ),
+            actions :
+            [
 
-            appBar : AppBar
+              IconButton
+              (
+                onPressed : ( )
+                {
+
+                  _pdfViewerController . jumpToPage ( 4 ) ;
+
+                },
+
+                icon : Icon ( Icons . arrow_drop_down_circle , color : Colors . white )
+
+              ),
+
+              IconButton
               (
 
-                actions :
-                [
+                onPressed : ( )
+                {
 
-                  IconButton
-                    (
-                      onPressed : ( )
-                      {
-                        _pdfViewerController . jumpToPage ( 4 ) ;
-                      },
-                      icon : Icon ( Icons . arrow_drop_down_circle , color : Colors . white )
-                  ),
+                  _pdfViewerController . zoomLevel = 1.25 ;
 
-                  IconButton
-                    (
-                      onPressed : ( )
-                      {
-                        _pdfViewerController . zoomLevel = 1.25 ;
-                      },
-                      icon : Icon ( Icons . zoom_in , color : Colors . white )
-                  )
+                },
 
-                ]
+                icon : Icon ( Icons . zoom_in , color : Colors . white )
 
-            )
+              )
+
+            ]
+
+          ),
+
+          body : SfPdfViewer . network
+          (
+
+            url! ,
+            controller : _pdfViewerController ,
+
+          )
 
         )
 
     );
+
   }
+  // Start Of build fun
 
 }
-
-/*
-class src
-{
-  late  String url    ;
-  src ( this .url  );
-  src . x ();
-
-
-}*/
+// End Of build fun
